f2d0271706bf99ccf925f4f472d20004c558adb7
emiliomoran <emiliojmp9@gmail.com>
2020-06-14 01:10:46 -0500

    FlatList feching data from github api

--- App.js
+++ App.js
@@ -1,6 +1,38 @@
 import React from "react";
+import { ApolloClient } from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { createHttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { InMemoryCache } from "apollo-cache-inmemory";
 import Navigation from "./app/navigations/Navigation";
+import { Provider as PaperProvider } from "react-native-paper";
+import { ACCESS_TOKEN } from "react-native-dotenv";
+
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = ACCESS_TOKEN;
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <ApolloProvider client={client}>
+      <PaperProvider>
+        <Navigation />
+      </PaperProvider>
+    </ApolloProvider>
+  );
 }
--- app/screens/Details.js
+++ app/screens/Details.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Text } from "react-native";
 
 const Details = () => {
-  return <div>Details...</div>;
+  return <Text>Details...</Text>;
 };
 
 export default Details;
